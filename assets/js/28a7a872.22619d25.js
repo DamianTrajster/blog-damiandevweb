68d6128de98cb27fb7e45b5ab82cf3cd3c061635
DamianTrajster <damiantrajster@gmail.com>
2024-07-26 22:55:25 -0300

    Deploy website - based on d77ebffd24f2f5f305435beb165862513f2900dd

---
+++ assets/js/28a7a872.22619d25.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[361],{7958:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/blog-damiandevweb/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/814f3328.32a8f97a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Aplicacion de Empleo AMdelCo","permalink":"/blog-damiandevweb/blog/APP Empleo","unlisted":false},{"title":"Softa!","permalink":"/blog-damiandevweb/blog/Softa!","unlisted":false}]}')}}]);
